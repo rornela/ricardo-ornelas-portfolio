41d0aa94b3e8b4140fcf938ce77082009060fdfc
Your Name <ricardoornelas02@gmail.com>
2025-08-24 01:14:27 -0700

    Fix GitHub Pages deployment with proper webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   
   // Fix for GitHub Pages deployment
-  if (env.mode === 'production') {
+  if (env.mode === 'production' || process.env.PUBLIC_URL) {
     config.output.publicPath = '/ricardo-ornelas-portfolio/';
   }
   
